824a45da57ca18608a4dd70db87d990d73175faf
Hyunjin Cha <164485306+hyunjin4270@users.noreply.github.com>
2025-05-16 10:48:10 +0900

    FEAT: 묵찌빠 구현

--- Rock-Scissors-Paper/src/rule/Mukchippa.java
+++ Rock-Scissors-Paper/src/rule/Mukchippa.java
@@ -4,36 +4,63 @@ import entity.Move;
 import entity.PlayContext;
 import entity.Player;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
-public class Mukchippa extends RockScissorPaper{
+public class Mukchippa extends RockScissorPaper {
+    private static final Random rand = new Random();
+
     @Override
     public String getName() {
         return "Mukchippa";
     }
 
+
+    /**
+     * 공격자와 같지 않은 행동을 취한 플레이어만 반환합니다.
+     * @param context 게임 정보
+     * @return 행동을 다르게 취한 수비자들
+     */
     @Override
     public Optional<List<Player>> play(PlayContext context) {
         Player attacker = context.getAttacker();
-        if (attacker == null) {
-            throw new IllegalArgumentException("attacker가 반드시 필요합니다.");
-        }
+        if (attacker == null) throw new IllegalArgumentException("공격자가 반드시 필요합니다.");
+
         Map<Player, Move> moves = context.getMoves();
         Move attMove = moves.get(attacker);
 
         List<Player> defenders = new LinkedList<>();
         for (Map.Entry<Player, Move> entry : moves.entrySet()) {
-            Player p = entry.getKey();
-            if (p.equals(attacker)) continue;
-
-            if (entry.getValue() != attMove) {
-                defenders.add(p);
-            }
+            Player player = entry.getKey();
+            if (player.equals(attacker)) continue;
+            if (entry.getValue() != attMove) defenders.add(player);
         }
 
         return Optional.of(defenders);
+    }
+
+    /**
+     * 만약 공격자를 이기는 행동을 취한 수비자가 다수일 때 사용하는 포인트입니다.
+     * @return 1부터 20까지 랜덤한 숫자
+     */
+    public int getAttackerPoint() {
+        return rand.nextInt(20) + 1;
+    }
+
+    /**
+     * 플레이어들의 주사위 눈금 중 가장 큰 눈금을 가진 플레이어를 반환합니다.
+     * 만약 가장 큰 눈금을 가진 플레이어가 다수일 시, 그 모두를 반환합니다.
+     * @param diceRolls 플레이어의 주사위 눈금
+     * @return 가장 큰 눈금을 가진 플레이어
+     */
+    public List<Player> defineAttacker(Map<Player, Integer> diceRolls) {
+        if (diceRolls == null || diceRolls.isEmpty()) throw new IllegalArgumentException("플레이어 포인트가 없습니다.");
+
+        Integer maximumPoint = Collections.max(diceRolls.values());
+        List<Player> attacker = new ArrayList<>();
+        for (Player player : diceRolls.keySet()) {
+            if (diceRolls.get(player).equals(maximumPoint)) attacker.add(player);
+        }
+
+        return attacker;
     }
 }
